fd6fd7004e5896fdd27f4a096538d27d0516271c
Aleksandra <aleksandra8mazur@gmail.com>
2019-06-04 20:21:40 +0200

    updated the loginpage.java file

--- Spotify_Project/src/test/java/pageobjects/LogInPage.java
+++ Spotify_Project/src/test/java/pageobjects/LogInPage.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 
 public class LogInPage extends BasePage{
 	
-	private String url = "https://accounts.spotify.com/pl/login/?continue=https:%2F%2Fwww.spotify.com%2Fuk%2Faccount%2Foverview%2F&_locale=en-GB";
+	private String url = "https://accounts.spotify.com/";
 	private String title = "Logowanie - Spotify";
 
 	public LogInPage(WebDriver driver) {
@@ -13,7 +13,7 @@ public class LogInPage extends BasePage{
 	}
 	
 	public LogInPage assertUrl() {
-		Assert.assertTrue(driver.getCurrentUrl().contains("https://accounts.spotify.com/"));
+		Assert.assertTrue(driver.getCurrentUrl().contains(url));
 		Assert.assertTrue(driver.getCurrentUrl().contains("login"));
 		
 		return this;
